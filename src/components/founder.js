7ff4b7d1c88ed078e1618a98d664915c715733d9
Diyor Mirpolatov <diyormirpolatov01@gmail.com>
2023-08-15 15:38:02 +0500

    about side is done

--- src/components/founder.js
+++ src/components/founder.js
@@ -11,12 +11,22 @@ function Founder() {
               <img
                 src="https://lh3.googleusercontent.com/pw/AIL4fc-9Y8mJj-XS0_Ba2HvsVuEEJ7xEQ90B6RbOq2d7Vt2pPlGPYJVeavQIXu-RbdZSu_uuMqCeALHEz1peCxNhD7CNlEI5At-DZvIQ8X7vIlDvS_RbHYixyVi9k9Wlugvx2MG7Lj5lBZcYAGGao3-NLjiH0h_quuRObnez5dvSwLkw_RJZPvwmUCr7eqsX-UC03BPqH_A9f1cMujNRzt79I794haEFfh0mvv_Rhry9zC5JDaI1hqIkQmsfNApVmh6zelqhIZEiEEEGbBS1fFCYWyFf-MO2EDBSPzRG6u0KdRfxuH6SgFR7DeEZAMc_Indu8fULpe2WXlfO8m2qm9V_W_wy0yEViFtF4ZUo-2elnJa84-XEtr-sxK9ewG8fWhYqrEaZ8Jyj_kUuN715vzxJX1hwkf_TvxZJ2PwAnSv6MaYBViXAEd5pcvc2q9O1eQRRcdnjzxEC9a-LnECt3qfL1XSA-J3PJvL9WPzmyjPqi_zs4N2ELdmhNOMg1Tz06kmDHFotT8QAGTvFUfzsc7hD5uGG96xCuYgIb_TTbT3NQzwbvMuhDBAe4_IDi9nfiwU3NyftYGm18mC-WcBbnGXzU9St7sfIbCnt4sV5C2juBHs6UZ8tng7YElfc8dfwCCBsNeyO-SD0COTZDsF4ndsNn62Ze18exs3W-3VqZVUFGjCTawtno4OYaeN2_Nl3ldwZy1wpjYrvV0wnTVnegLaZBoIHbiYmRNPM9qDtEnkLwZsEFI91Qz8hbjJ1qVm9Ibi3zOb8Ao1OcvukMEiVrdGTJOB95Ex3VzJaUgEptLAfQZ1an_ZozVtQ_7MbTwtMjFOekxF5dJcQMXldig_07DCo86x87rW7zSRLhf6CM26pQWvns3EpHa2gZ7HTWtwjb3TxxSuJ1NijFjBxUvpYkmMH=w934-h934-s-no?authuser=0"
                 alt=""
-                width={500}
-                height={500}
               />
             </figure>
           </div>
-          <div className="about"></div>
+          <div className="about">
+            <h1>Mirpo'latov Davron</h1>
+            <h3>Artwood asoschisi</h3>
+            <p>
+              Mirpo'latov Davron 2004-yil oddiy oilada tug'ilgan. Yoshligidan
+              hunarga chanqoq bo'lib 8 yoshidan beri o'zini avval
+              hunarmandchilik sohasida so'ngra, o'ymakorlik sohasida sinab
+              kelyapti. Bir qancha yirik loyihalar jumladan yangi masjidlarning
+              yog'och rom va eshiklarini o'ygan, 200 dan ortiq laganlarni ommaga
+              taqdim etgan. Hozirda 19 yosh bolishiga qaramay, juda ko'p sifatli
+              yog'och buyumlar va anjomlarning egasidir.
+            </p>
+          </div>
         </div>
       </div>
     </>
